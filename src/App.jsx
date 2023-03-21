6eb2b8747951554967540663ffe70d65e865b098
VijayKumar4003 <38779300+VijayKumar4003@users.noreply.github.com>
2023-03-21 19:18:51 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import HeaderCo from './assets/components/HeaderCo'
+import './App.css'
+
+function App() {
+ 
+  return (
+    
+     <HeaderCo />
+   
+  )
+}
+
+export default App
---
+++ src/assets/components/HeaderCo.jsx
@@ -0,0 +1,12 @@
+function HeaderCo() {
+  
+  return (
+    <div>
+
+        <h2>Hello This Project is Created with Vite</h2>
+      
+    </div>
+  )
+}
+
+export default HeaderCo;
